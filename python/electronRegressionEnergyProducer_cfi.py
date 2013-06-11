0f3340c156b919f497f229b647b0a06d20309f60
Sangeun Lee <sha1-3a6af270891ea871cb501f2986c1d5ffd1a70d5c@cern.ch>
2013-06-11 23:59:58 +0000

    *** empty log message ***

--- python/electronRegressionEnergyProducer_cfi.py
+++ python/electronRegressionEnergyProducer_cfi.py
@@ -11,7 +11,8 @@ eleRegressionEnergy = cms.EDProducer("RegressionEnergyPatElectronProducer",
                                      # produce ValueMaps. Should be true for GsfElectrons otherwise this producer doest nothing. Keep it to false for PAT
                                    produceValueMaps = cms.bool(False),
                                    #regressionInputFile = cms.string("EgammaAnalysis/ElectronTools/data/eleEnergyRegWeights_V1.root"),
-                                   regressionInputFile = cms.string("EgammaAnalysis/ElectronTools/data/eleEnergyRegWeights_WithSubClusters_VApr15.root"),
+                                   regressionInputFile = cms.string("EgammaAnalysis/ElectronTools/data/eleEnergyReg2011Weights_WithSubClusters_VApr23.root"),
+                                   #regressionInputFile = cms.string("EgammaAnalysis/ElectronTools/data/eleEnergyRegWeights_WithSubClusters_VApr15.root"),
                                      ## Regression type (1: ECAL regression w/o subclusters 2: ECAL regression w/ subclusters)
                                    #energyRegressionType = cms.uint32(1),
                                    energyRegressionType = cms.uint32(2),
